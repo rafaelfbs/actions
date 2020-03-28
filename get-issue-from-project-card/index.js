2b16e0b0eca23627a7d1d7955355fddb4101f5e6
Rafael Ferrari <me@rferrari.net>
2020-03-28 08:20:39 -0300

    add get-issue-from-project-card action

---
+++ get-issue-from-project-card/index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+function extractIssueFromCardContentUrl(contentUrl) {
+    const match = url.match(/issues\/(\d+)$/);
+	if (!match) {
+		throw new Error('Failed to get issue id');
+	}
+
+	return parseInt(match[1], 10);
+}
+
+async function run() {
+    try {
+        const token = core.getInput('token', { required: true });
+        const cardId = core.getInput('card',  { required: true });
+
+        const client = new github.GitHub(token);
+        const card = await client.projects.getCard({ card_id: cardId });
+        const issueId = extractIssueFromCardContentUrl(card.data.content_url);
+
+        core.setOutput('issue', issueId.toString());
+    } catch (error) {
+        core.error(error);
+        core.setFailed(error.message);
+    }
+}
+
+run();
